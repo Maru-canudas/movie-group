e127114f20343ed28279c89843547b6c040763df
Mariela Canudas <mariela@poincenot.com>
2019-11-27 13:11:06 -0300

    X-X | link login

--- app/app.module.js
+++ app/app.module.js
@@ -1,10 +1,15 @@
 'use strict';
 
 // Define the `movieGroupApp` module
-let movieGroupApp = angular.module('movieGroupApp', [
+angular.module('movieGroupApp', [
   'ngAnimate',
   'ngRoute',
   'ngMaterial',
   'ngMessages'
+]).controller('appController', ['$scope',
+    function ($scope) {
+        $scope.loginNext = function(){
+            window.location.href = '#!/seats';
+        };
+    }
 ]);
-
